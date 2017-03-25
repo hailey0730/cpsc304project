4cb89a677288ae3a55234b364aa636d807c5b788
hailey0730 <hayhay0730@gmail.com>
2017-03-24 18:14:19 -0700

    create new java project

---
+++ src/UserInterface.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 3/24/2017.
+ */
+
+import java.lang.String;
+import java.io.IOException;
+import java.util.*;
+
+public class UserInterface {
+
+    Connection con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "<myusername>", "<mypassword>");       // change username, password
+
+    public static void main(String[]args){}
+}
