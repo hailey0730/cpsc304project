c5e999d46ea17c9dd00062f3fbd7ba60815f067e
Gulipek Candan <gulipek@dhcp-128-189-195-57.ubcsecure.wireless.ubc.ca>
2017-03-28 15:03:45 -0700

    deleted classes12 - it was old

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,12 +3,25 @@
  */
 
 import java.lang.String;
-import java.io.IOException;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class UserInterface {
 
-    Connection con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "<myusername>", "<mypassword>");       // change username, password
 
-    public static void main(String[]args){}
+    public static void main(String[]args){
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:ug", "ora_d4c9", "a53906137");       // change username, password
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("hello");
+
+    }
 }
